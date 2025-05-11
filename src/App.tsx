b3c1d7efb9c88e6607e9c94e5c6d2fa1c0a7df2e
nela <kknela44@gmail.com>
2025-05-11 22:45:35 +0200

    Poprawiono null w confirmation.html

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CartPage from './pages/CartPage';
 import ProductListPage from './pages/ProductListPage';
 import SummaryPage from './pages/SummaryPage';
-import ConfirmationPage from './pages/ConfirmationPage';
+
 
 function App() {
 
@@ -16,7 +16,6 @@ function App() {
           <Route path="/" element={<ProductListPage />} />
           <Route path="/Cart" element={<CartPage />} />
           <Route path="/Summary" element={<SummaryPage />} />
-          <Route path="/Confirmation" element={<ConfirmationPage />} />
         </Routes>
       </Router>
       
